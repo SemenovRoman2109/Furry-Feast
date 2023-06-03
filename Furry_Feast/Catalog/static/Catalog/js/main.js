da2c74e19cf247e1d0781955bb26dfbeda33ce0b
Roman <romansemenov2109@gmail.com>
2023-06-03 22:37:23 +0300

    add-filter-css-search

--- Furry_Feast/Catalog/static/Catalog/js/main.js
+++ Furry_Feast/Catalog/static/Catalog/js/main.js
@@ -9,10 +9,18 @@ function sendSelectCategory(kind,animal){
         $.ajax({
             type: "POST",
             url: $(".all-filter").action,
-            data: { csrfmiddlewaretoken: document.getElementsByName("csrfmiddlewaretoken")[0].value,
-                "animal":animal.textContent,
-                "kind":kind.textContent,
+            data: { csrfmiddlewaretoken: document.getElementsByName("csrfmiddlewaretoken")[0].value,"animal":animal.textContent,"kind":kind.textContent,"text": "none"},
+            success: function(response){
+                let listAllProduct = document.querySelectorAll(".section")
+                listAllProduct.forEach(function(product,index,listAllProduct){
+                    product.style.display = "flex";
+                })
+                listAllProduct.forEach(function(product,index,listAllProduct){
+                    if (! response.products.includes(Number(product.id.split("-")[1])) ){
+                        product.style.display = "none";
                     }
+                })
+            }
                     
         });
         
@@ -21,22 +29,89 @@ function sendSelectCategory(kind,animal){
 
 listKind.forEach(function(kind,index,listKind){
     kind.addEventListener("click",function(event){
-        listKind.forEach(function(anotherKind,index,listKind){
-            anotherKind.style.color = "#DFDFDF";
-        })
-        kind.style.color = "#FFC700";
-        selectKind = kind;
-        sendSelectCategory(selectKind,selectAnimal)
+        if (! kind.classList.contains("select-category")){
+            listKind.forEach(function(anotherKind,index,listKind){
+                anotherKind.style.color = "#DFDFDF";
+                anotherKind.classList.remove("select-category")
+            })
+            kind.style.color = "#FFC700";
+            kind.classList.add("select-category")
+            selectKind = kind;
+            sendSelectCategory(selectKind,selectAnimal)
+        }
+        else{
+            kind.style.color = "#DFDFDF";
+            kind.classList.remove("select-category")
+            selectKind = "none";
+            sendSelectCategory(selectKind,selectAnimal)
+        }
+        
     })
 })
     
 listAnimal.forEach(function(animal,index,listAnimal){
     animal.addEventListener("click",function(event){
-        listAnimal.forEach(function(anotherAnimal,index,listAnimal){
-            anotherAnimal.style.color = "#DFDFDF";
+        if (! animal.classList.contains("select-category")){
+            listAnimal.forEach(function(anotherAnimal,index,listAnimal){
+                anotherAnimal.style.color = "#DFDFDF";
+                anotherAnimal.classList.remove("select-category")
+            })
+            animal.style.color = "#FFC700";
+            animal.classList.add("select-category")
+            selectAnimal = animal;
+            sendSelectCategory(selectKind,selectAnimal)
+        }
+        else{
+            animal.style.color = "#DFDFDF";
+            animal.classList.remove("select-category")
+            selectAnimal = "none";
+            sendSelectCategory(selectKind,selectAnimal)
+        }
+    })
+    
+})
+
+
+$(document).ready(function () {
+    $(".input-search").on("submit", function (event) {
+        event.preventDefault();
+        let text =  document.querySelector(".input-field").value;
+        $.ajax({
+            type: "POST",
+            url: $(this).action,
+            data: { csrfmiddlewaretoken: document.getElementsByName("csrfmiddlewaretoken")[0].value,"animal":"none","kind":"none","text":text},
+            success: function(response){
+                let listAllProduct = document.querySelectorAll(".section")
+                listAllProduct.forEach(function(product,index,listAllProduct){
+                    product.style.display = "flex";
+                })
+                listAllProduct.forEach(function(product,index,listAllProduct){
+                    if (! response.products.includes(Number(product.id.split("-")[1])) ){
+                        product.style.display = "none";
+                    }
+                })
+            }
+
+        });
+    })
+});
+
+let buttonsBasket = document.querySelectorAll(".button-basket");
+buttonsBasket.forEach(function(buttonBasket,index,buttonsBasket){
+    buttonBasket.addEventListener("click",function(event){
+        let modalWindow = document.querySelector(".modal-window-add-cart");
+        let coverDiv = document.createElement('div'); 
+        coverDiv.classList.add('cover-div'); 
+        document.body.append(coverDiv);
+        modalWindow.style.display = "flex";
+        setTimeout(()=>{modalWindow.style.opacity = 1;},10)
+
+        coverDiv.addEventListener("click",function(event){
+            let coverdiv = document.querySelector('.cover-div');
+            coverdiv.remove();
+            modalWindow.style.opacity = 0;
+            setTimeout(()=>{modalWindow.style.display = "none";},1000)
         })
-        animal.style.color = "#FFC700";
-        selectAnimal = animal;
-        sendSelectCategory(selectKind,selectAnimal)
+        
     })
 })
--- Furry_Feast/Catalog/views.py
+++ Furry_Feast/Catalog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import *
 from django.shortcuts import get_object_or_404
+from django.http import JsonResponse
 
 # Create your views here.
 def show_main(request):
@@ -8,8 +9,28 @@ def show_main(request):
     if request.method == 'POST':
         kind = request.POST.get("kind")
         animal = request.POST.get("animal")
+        text = request.POST.get("text")
+        if text != "none":
+            filtered_products = Product.objects.filter(name__icontains=text)
+        else:
+            if kind != None and animal == None:
+                filtered_products = Product.objects.filter(category__name=kind)
+            elif animal != None and kind == None:
+                filtered_products = Product.objects.filter(category__name=animal)
+            elif animal != None and kind != None:
+                filtered_products = Product.objects.filter(category__name=animal).filter(category__name=kind)
+            elif animal == None and kind == None:
+                filtered_products = Product.objects.all()
+
+
+        products_list = []
         
-        print(kind, animal)
+        for product in filtered_products:
+            products_list.append(product.pk)
+        
+        return JsonResponse({"products": products_list})
+    
+
     return render(request,"Catalog/main.html", context)
 
 def show_product(request, product_pk):
